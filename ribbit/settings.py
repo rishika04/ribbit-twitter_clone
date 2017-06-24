cb5b4256ff9330d247a9dac54803ddbcaa35c168
rishika04 <srishika214@gmail.com>
2017-06-25 02:09:41 +0530

    implemented login and signup

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ribbit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_PATH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'ribbit_app/static')
 
 STATIC_URL = '/static/'
+
--- ribbit/urls.py
+++ ribbit/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from ribbit_app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index), # root
+    url(r'^login$', views.login_view), # login
+    url(r'^logout$', views.logout_view), # logout
+    url(r'^signup$', views.signup), # signup
 ]
